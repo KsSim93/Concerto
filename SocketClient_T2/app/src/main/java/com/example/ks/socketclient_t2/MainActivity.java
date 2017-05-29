dd58142c46c63e70084671f76448369a809d40ad
DESKTOP-4JKCTPR\KS <93sks@naver.com>
2017-05-29 19:01:22 +0900

    안드로이드 소켓통신 클라이언트

---
+++ SocketClient_T2/app/src/main/java/com/example/ks/socketclient_t2/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.ks.socketclient_t2;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView recieveText;
+    EditText editTextAddress, editTextPort;
+    Button connectBtn;
+
+    Socket socket = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //앱 기본 스타일 설정
+        getSupportActionBar().setElevation(0);
+
+        connectBtn = (Button) findViewById(R.id.buttonConnect);
+
+        editTextAddress = (EditText) findViewById(R.id.addressText);
+        editTextPort = (EditText) findViewById(R.id.portText);
+        recieveText = (TextView) findViewById(R.id.textViewRecieve);
+
+
+        //connect 버튼 클릭
+        connectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyClientTask myClientTask = new MyClientTask(editTextAddress.getText().toString(), Integer.parseInt(editTextPort.getText().toString()));
+                myClientTask.execute();
+            }
+        });
+
+    }
+
+    //
+    public class MyClientTask extends AsyncTask<Void, Void, Void> {
+        String dstAddress;
+        int dstPort;
+        String response = "";
+        String myMessage = "";
+
+        //constructor
+        MyClientTask(String addr, int port){
+            dstAddress = addr;
+            dstPort = port;
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            Socket socket = null;
+            myMessage = myMessage.toString();
+            try {
+                socket = new Socket(dstAddress, dstPort);
+                //송신
+                OutputStream out = socket.getOutputStream();
+                out.write(myMessage.getBytes());
+
+                //수신
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                InputStream inputStream = socket.getInputStream();
+
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    byteArrayOutputStream.write(buffer, 0, bytesRead);
+                    response += byteArrayOutputStream.toString("UTF-8");
+                }
+
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                response = "UnknownHostException: " + e.toString();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                response = "IOException: " + e.toString();
+            }finally{
+                if(socket != null){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            recieveText.setText(response);
+            super.onPostExecute(result);
+        }
+    }
+
+}
